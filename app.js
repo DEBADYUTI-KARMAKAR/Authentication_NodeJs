de82281f96cae60bfa55f838c2182ab6ed5e4881
unknown <debadyuti.karmakar@gmail.com>
2022-10-21 01:56:35 +0530

    Upto db connect

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 require("dotenv").config();
+require("./config/database").connect();
 const express = require('express');
-
+const User = require('./model/user');
 const app=express();
+app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
+
+/*
+Register user
+
+get all information
+check mandatory fields
+already registered
+takecare of password
+generate token or success message
+
+*/
+
+app.post("/register", async (req, res) => {
+    const {firstName,lastName,email,password}=req.body;
+    if(!(firstName && lastName && email && password)){
+        res.status(400).send("All input is required");
+    }
+    const existingUser = await User.findOne({email}); // Promise
+    if(existingUser){
+        res.status(400).send("User already exist. Please login");
+    }
+    const user = User.create({
+        firstName,
+        lastName,
+        email:email.toLowerCase(),
+        password
+    });
+    return res.status(201).json(user);
+});
+
+
 
 module.exports=app;
---
+++ config/database.js
@@ -0,0 +1,17 @@
+const { default: mongoose } = require("mongoose");
+
+const {MONGODB_URL}=process.env;
+
+exports.connect =()=>{
+    mongoose.connect(MONGODB_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        
+    }).then(console.log('Database connected successfully'))
+    .catch((err)=>{
+        console.log(err);
+        process.exit(1);
+    }
+    );
+    
+}
